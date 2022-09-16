6918c823b2c3b3e9e328c24f3275e198a56eee17
Patrick Brus <brus.patrick63@gmail.com>
2022-09-16 17:10:53 +0200

    Feat: Made s3 bucket public read again2

--- aws_infra_setup/create-and-upload-s3.sh
+++ aws_infra_setup/create-and-upload-s3.sh
@@ -5,7 +5,7 @@ AWS_ID=$(aws sts get-caller-identity --query Account --output text | cat)
 AWS_REGION=$(aws configure get region)
 
 echo "Creating bucket "
-aws s3api create-bucket --acl public-read-write --bucket $BUCKET_NAME --create-bucket-configuration LocationConstraint=$AWS_REGION 
+aws s3api create-bucket --acl public-read --bucket $BUCKET_NAME --create-bucket-configuration LocationConstraint=$AWS_REGION 
 
 echo "Enable versioning"
 aws s3api put-bucket-versioning --bucket $BUCKET_NAME --versioning-configuration Status=Enabled
